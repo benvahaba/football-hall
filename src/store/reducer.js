5b2dd9be105ae3b65f706c6ba681843c0fbe5d32
benvahaba <51039453+benvahaba@users.noreply.github.com>
2022-03-02 16:45:57 +0200

    first commit

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  teams: [],
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "UPDATE": {
+    }
+  }
+}
+
+export default reducer;
